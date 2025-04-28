d4f27c55801632ff91a3daa6a34fddea24e54d11
uj <aulien.jbadji@gmail.com>
2025-04-28 08:41:18 +0200

    add serde feature

--- src/lib.rs
+++ src/lib.rs
@@ -136,6 +136,7 @@ impl Display for MagnetError {
 impl Error for MagnetError {}
 
 #[derive(Debug, Clone, Hash, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Magnet {
     /// Display Name of the torrent
     pub display_name: Option<String>,
